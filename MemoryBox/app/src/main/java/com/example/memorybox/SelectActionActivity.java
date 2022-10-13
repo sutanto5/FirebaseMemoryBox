3f14ec9a2327da2c45c0799c06e4dc58384f5878
sutanto7475 <76191538+sutanto7475@users.noreply.github.com>
2022-10-13 08:18:06 -0500

    10/13/22 CATCH UP

--- MemoryBox/app/src/main/java/com/example/memorybox/SelectActionActivity.java
+++ MemoryBox/app/src/main/java/com/example/memorybox/SelectActionActivity.java
@@ -2,13 +2,34 @@ package com.example.memorybox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SelectActionActivity extends AppCompatActivity {
 
+    public final String TAG = "Denna";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_action);
     }
+
+    public void showMemories(View view) {
+
+    }
+
+    public void addMemories(View view) {
+        Intent intent = new Intent(SelectActionActivity.this, AddMemoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void logOutClicked(View view) {
+        SignInActivity.firebaseHelper.logOutUser();
+        Log.i(TAG, "user logged out");
+        Intent intent = new Intent(SelectActionActivity.this, SignInActivity.class);
+        startActivity(intent);
+    }
 }
+
--- MemoryBox/app/src/main/java/com/example/memorybox/SignInActivity.java
+++ MemoryBox/app/src/main/java/com/example/memorybox/SignInActivity.java
@@ -1,7 +1,8 @@
 package com.example.memorybox;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,44 +10,163 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity  {
 
+    // xml elements
     Button logInB, signUpB;
     EditText userNameET, passwordET;
+
+    // values to pass to firebase auth for log in/sign up
+    String userName, password;
+
+    // this variable will be used for communicating with Firebase auth and firestore db
+    public static FirebaseHelper firebaseHelper;
+
+    // Use the same TAG all the time for Log statements. Feel free to change the value of TAG
+    public final String TAG = "Denna";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        logInB = findViewById(R.id.login);
-        signUpB = findViewById(R.id.SignUp);
-        userNameET = findViewById(R.id.name);
-        passwordET = findViewById(R.id.desc);
+        firebaseHelper = new FirebaseHelper();
+        logInB = findViewById(R.id.logInButton);
+        signUpB = findViewById(R.id.signUpButton);
+        userNameET = findViewById(R.id.userNameEditText);
+        passwordET = findViewById(R.id.passwordEditText);
+    }
+
+    /**
+     * Method first checks if the input is valid.  If it meets the screening criteria from
+     * getValues(), then the username (which is an email) and password are sent to the FirebaseHelper
+     * class to call upon a method of Firebase auth called createUserWithEmailAndPassword
+     *
+     * This method will return a result once it is complete, and we are listening for that result
+     *
+     * No matter what, the method will complete, either successfully or not
+     * If successfully, that means the user account was created and we can now access the UID of
+     * the signed in user.
+     * After doing that we switch screens to the SelectActionActivity
+     *
+     * If unsuccessful, then we will Log that we failed, and include the exception message which will help
+     * us gain insight into WHY it didn't work.
+     *
+     * @param view
+     */
+
+
+    public void signUpClicked(View view) {
+        Log.i(TAG, "Sign up clicked");
+        if (getValues()) {      // get username and password
+            // Try to create an account using auth
+            firebaseHelper.getmAuth().createUserWithEmailAndPassword(userName, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // FOR SOME REASON IF THE CREATE USER IS NOT WORKING, THIS IS CRASHING
+                            // NOT SURE WHY?  I TRIED WITH A DUPLICATE EMAIL ADDRESS AND THAT CRASHED IT.
+                            // LOGCAT SHOWED THE MESSAGE BUT I COULDN'T GET IT TO SHOW IN A LOG STATEMENT
+
+                            if (task.isSuccessful()){
+                                // Sign up successful, update UI with the currently signed in user's info
+                                firebaseHelper.updateUid(firebaseHelper.getmAuth().getUid());
+                                Log.d(TAG, userName + " created and logged in");
+
+                                // we will implement this later
+                                // updateIfLoggedIn();
+                                // firebaseHelper.attachReadDataToUser();
+
+                                Intent intent = new Intent(SignInActivity.this, SelectActionActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                // if sign up fails, display a message to the user along with the exception from firebase auth
+                                Log.d(TAG, "Sign up failed for " + userName + " " + password +
+                                        " because of \n"+ task.getResult());
+
+                            }
+                        }
+                    });
+        }
+        else {
+   /*
+   This prevents the app from CRASHING when the user enters bad items
+   (duplicate email or badly formatted email most likely)
+
+   https://stackoverflow.com/questions/37859582/how-to-catch-a-firebase-auth-specific-exceptions
+
+    */
+
+            try {
+                throw task.getException();
+            } catch (FirebaseAuthInvalidCredentialsException e) {
+                // poorly formatted email address
+                Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Sign up failed for " + userName + " " + password + e.getMessage());
+            } catch (FirebaseAuthEmailException e) {
+                // duplicate email used
+                Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Sign up failed for " + userName + " " + password + e.getMessage());
+            } catch (Exception e) {
+                Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Sign up failed for " + userName + " " + password + e.getMessage());
+            }
+
+
+            // this log message will tell the name of the exception.  If you want to add this to the catch
+            // statement above, then just add another catch above the generic one at the end
+
+            Log.d(TAG, "Sign up failed for " + userName + " " + password +
+                    " because of \n"+ task.getException());
+        }
+
+
     }
 
     public void logInClicked(View view) {
-        Log.i("Denna", "Log in clicked");
+        Log.i(TAG, "Log in clicked");
         if (getValues()) {        // get username and password
-            // check if valid with Firebase auth
             // if valid, log in user and then switch to next activity
+            // Try to sign into an account using auth with given email and password
+            firebaseHelper.getmAuth().signInWithEmailAndPassword(userName, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                // Sign in success, update currently signed in user's info
+                                firebaseHelper.updateUid(firebaseHelper.getmAuth().getUid());
 
-            Intent intent = new Intent(SignInActivity.this, SelectActionActivity.class);
-            startActivity(intent);
+                                // we will implement this later
+                                // updateIfLoggedIn();
+                                // firebaseHelper.attachReadDataToUser();
+
+                                Log.d(TAG, userName + " logged in");
+
+                                Intent intent = new Intent(SignInActivity.this, SelectActionActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                // if log in fails, display a message to the user along with the exception from firebase auth
+                                Log.d(TAG, "Log in failed for " + userName + " " + password +
+                                        " because of \n"+ task.toString());
+                            }
+                        }
+                    });
+
+
+
 
             // if invalid - prompt message that says why signin won't go through
         }
     }
 
-    public void signUpClicked(View view) {
-        Log.i("Denna", "Sign up clicked");
-        if (getValues()) {      // get username and password
-            // Try to create an account using auth
-            // if successful, switch to next activity
-
-        }
-    }
 
     /**
      * Helper method to get userName and password whenever one of these buttons is clicked
@@ -57,8 +177,8 @@ public class SignInActivity extends AppCompatActivity {
      * @return true if values pass these tests, false otherwise
      */
     private boolean getValues() {
-        String userName = userNameET.getText().toString();
-        String password = passwordET.getText().toString();
+        userName = userNameET.getText().toString();
+        password = passwordET.getText().toString();
 
         // verify all user data is entered
         if (userName.length() == 0 || password.length() == 0) {
@@ -70,11 +190,31 @@ public class SignInActivity extends AppCompatActivity {
         else if (password.length() < 6) {
             Toast.makeText(getApplicationContext(), "Password must be at least 6 char long", Toast.LENGTH_SHORT).show();
             return false;
-        } else {
-            Log.i("Denna", userName + " " + password + " is set after getValues(), return true");
+        }else {
+            Log.i(TAG, userName + " " + password + " is set after getValues(), return true");
+            userName = removeTrailingSpaces(userName);
             return true;
         }
+
     }
 
+    /**
+     * This method accepts the email the user wants to submit for FirebaseAuth
+     * and removes an extra spaces that may have accidentally been added at the end by
+     * the auto-correct keyboard.  This typically happens when the email is used all
+     * the time and shows up as a suggestion for the user.
+     *
+     * @param email
+     * @return a String without trailing spaces
+     */
+    private String removeTrailingSpaces(String email) {
+        String lastChar = email.substring(email.length() -1);
+        if (lastChar.equals(" "))
+            email = email.substring(0, email.length()-1);
+        return email;
+    }
+
+
 }
+
 
